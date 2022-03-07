a22e8d2abd04af6550da1a27d1d09cc891c8129a
Cem Freimoser <cem.freimoser@egym.com>
2022-03-07 01:28:25 +0100

    main: Added renamed file

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -16,7 +16,7 @@ class Constants {
   // images
   static const String content_cover_image = "assets/images/Content-cover.jpeg";
   static const String netflix_background_image =
-      "assets/images/Background-image.jpeg";
+      "assets/images/Login-cover.jpeg";
 
   // colors
   static Color netflix_red = Color.fromRGBO(229, 9, 20, 1);
