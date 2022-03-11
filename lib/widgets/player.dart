163735504da652bff212f8d7f3e0e83cbaee2b8a
Cem Freimoser <cem.freimoser@egym.com>
2022-03-11 18:31:15 +0100

    main: Added touch support

--- lib/widgets/player.dart
+++ lib/widgets/player.dart
@@ -94,6 +94,15 @@ class _overlayControlState extends State<_overlayControl> {
   }
 
   @override
+  void deactivate() {
+    super.deactivate();
+
+    if (_timer != null && _timer!.isActive) {
+      _timer!.cancel();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     var _desktopOverlay = Stack(
       children: [
@@ -207,119 +216,117 @@ class _overlayControlState extends State<_overlayControl> {
       ],
     );
 
-    var _mobileOverlay = GestureDetector(
-        onTap: () => setState(() {
-              _hideOverlay = true;
-            }),
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                margin: const EdgeInsets.only(left: 16, right: 16, top: 54),
-                child: Row(
-                  children: [
-                    IconButton(
-                      padding: EdgeInsets.zero,
-                      onPressed: () => log("search"),
-                      icon: Icon(Icons.arrow_back),
-                      iconSize: 60,
-                      color: Colors.white,
-                    ),
-                    Expanded(
-                        child: Text(
-                      widget.content.title,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(color: Colors.white),
-                    )),
-                    IconButton(
-                      padding: EdgeInsets.zero,
-                      onPressed: () => log("search"),
-                      icon: Icon(Icons.flag),
-                      iconSize: 60,
-                      color: Colors.white,
-                    )
-                  ],
+    var _mobileOverlay = Stack(
+      children: [
+        Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            margin: const EdgeInsets.only(left: 16, right: 16, top: 54),
+            child: Row(
+              children: [
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () => log("search"),
+                  icon: Icon(Icons.arrow_back),
+                  iconSize: 60,
+                  color: Colors.white,
                 ),
-              ),
+                Expanded(
+                    child: Text(
+                  widget.content.title,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                )),
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () => log("search"),
+                  icon: Icon(Icons.flag),
+                  iconSize: 60,
+                  color: Colors.white,
+                )
+              ],
             ),
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                margin: const EdgeInsets.only(left: 64, right: 64),
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () => log("search"),
-                    icon: Icon(Icons.fast_rewind),
-                    iconSize: 60,
-                    color: Colors.white,
-                  ),
-                  Spacer(),
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () => log("search"),
-                    icon: Icon(Icons.play_arrow),
-                    iconSize: 60,
-                    color: Colors.white,
-                  ),
-                  Spacer(),
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () => log("search"),
-                    icon: Icon(Icons.fast_forward_rounded),
-                    iconSize: 60,
-                    color: Colors.white,
-                  ),
-                ]),
+          ),
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+            margin: const EdgeInsets.only(left: 64, right: 64),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              IconButton(
+                padding: EdgeInsets.zero,
+                onPressed: () => log("search"),
+                icon: Icon(Icons.fast_rewind),
+                iconSize: 60,
+                color: Colors.white,
               ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                height: 100,
-                color: Colors.black,
-                child: Row(
-                  children: [
-                    VolumeButton(
-                      volume: volume,
-                      onVolumeChanged: (double value) {
-                        setState(() {
-                          volume = value;
-                          widget.controller.setVolume(value);
-                        });
-                      },
-                    ),
-                    Expanded(
-                        child: VideoProgressIndicator(widget.controller,
-                            allowScrubbing: true)),
-                    IconButton(
-                      padding: EdgeInsets.zero,
-                      onPressed: () => log("search"),
-                      icon: Icon(Icons.av_timer),
-                      iconSize: 60,
-                      color: Colors.white,
-                    ),
-                    IconButton(
-                      padding: EdgeInsets.zero,
-                      onPressed: () => log("search"),
-                      icon: Icon(Icons.fullscreen),
-                      iconSize: 60,
-                      color: Colors.white,
-                    ),
-                  ],
+              Spacer(),
+              IconButton(
+                padding: EdgeInsets.zero,
+                onPressed: () => log("search"),
+                icon: Icon(Icons.play_arrow),
+                iconSize: 60,
+                color: Colors.white,
+              ),
+              Spacer(),
+              IconButton(
+                padding: EdgeInsets.zero,
+                onPressed: () => log("search"),
+                icon: Icon(Icons.fast_forward_rounded),
+                iconSize: 60,
+                color: Colors.white,
+              ),
+            ]),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 100,
+            color: Colors.black,
+            child: Row(
+              children: [
+                VolumeButton(
+                  volume: volume,
+                  onVolumeChanged: (double value) {
+                    setState(() {
+                      volume = value;
+                      widget.controller.setVolume(value);
+                    });
+                  },
                 ),
-              ),
+                Expanded(
+                    child: VideoProgressIndicator(widget.controller,
+                        allowScrubbing: true)),
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () => log("search"),
+                  icon: Icon(Icons.av_timer),
+                  iconSize: 60,
+                  color: Colors.white,
+                ),
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () => log("search"),
+                  icon: Icon(Icons.fullscreen),
+                  iconSize: 60,
+                  color: Colors.white,
+                ),
+              ],
             ),
-          ],
-        ));
+          ),
+        ),
+      ],
+    );
 
-    return MouseRegion(
-      onHover: (event) => _showOverlay(),
-      child: _hideOverlay
-          ? Container()
-          : AdaptiveLayout(mobile: _mobileOverlay, desktop: _desktopOverlay),
+    return GestureDetector(
+      onTap: () => _showOverlay(),
+      child: MouseRegion(
+        onHover: (event) => _showOverlay(),
+        child: _hideOverlay
+            ? Container()
+            : AdaptiveLayout(mobile: _mobileOverlay, desktop: _desktopOverlay),
+      ),
     );
   }
 }
